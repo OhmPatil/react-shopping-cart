f33d0a877c0694ad3845a853691d619f75567180
OhmPatil <workpatil10@gmail.com>
2022-08-18 04:03:10 +0530

    Create sections on cart page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,22 +1,28 @@
-import '../styles/cart.css'
-import ItemCard from '../components/ItemCard';
+import "../styles/cart.css";
+import ItemCard from "../components/ItemCard";
 
 const Cart = (props) => {
-    let items = props.cartItems
-    console.log('fromCART', items);
-    return(
-        <div id='cart'>
-            {items.map(item => {
-                return(
-                    <ItemCard image={item.image}
-                    id={item.id}
-                    title={item.title}
-                    price={item.price}
-                    />
-                )
-            })}
-        </div>
-    )
-}
+  let items = props.cartItems;
+  console.log("fromCART", items);
+  return (
+    <div id="cart">
+      <div className="product-section">
+        {items.map((item) => {
+          return (
+            <ItemCard
+              image={item.image}
+              id={item.id}
+              title={item.title}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+      <div className="payment-section">
+        <h1>Payment stuff</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
