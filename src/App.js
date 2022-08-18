b39b7b9a073dfff38cdfd260b0c819637511bac2
OhmPatil <workpatil10@gmail.com>
2022-08-18 16:35:33 +0530

    Restructure code, fetch data in Appjs itself

--- src/App.js
+++ src/App.js
@@ -5,11 +5,42 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Store from "./pages/store";
 import Cart from "./pages/cart";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import fetchItem from "./utils/FetchItem";
 
 const App = () => {
   const [cartItems, setCartItems] = useState([])
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("https://fakestoreapi.com/products/", {
+      mode: "cors",
+    });
+    const data = await response.json();
+
+    return data;
+  }; 
+
+  useEffect(() => {
+    async function loadData() {
+      let reqData = [];
+      let allData = await fetchData();
+
+      allData.forEach((item) => {
+        if (
+          item.category === "men's clothing" ||
+          item.category === "women's clothing"
+        ) {
+          reqData.push(item);
+        }
+      });
+
+      setProducts(reqData);
+    }
+
+    loadData();
+  }, []);
+
 
   const handleAddtoCart = async (e, quantity) => {
     let item = await fetchItem(e.target.id)
@@ -26,7 +57,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/store" element={<Store handleAddtoCart={handleAddtoCart}/>} />
+          <Route path="/store" element={<Store handleAddtoCart={handleAddtoCart} products={products}/>} />
           <Route path="/cart" element={<Cart cartItems={cartItems}/>} />
         </Routes>
         <Footer />
--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -1,44 +1,14 @@
 import "../styles/store.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import CardContainer from "../components/CardContainer";
 
 const Store = (props) => {
-  const [products, setProducts] = useState([]);
 
-  const fetchData = async () => {
-    const response = await fetch("https://fakestoreapi.com/products/", {
-      mode: "cors",
-    });
-    const data = await response.json();
-
-    return data;
-  }; 
-
-  useEffect(() => {
-    async function loadData() {
-      let reqData = [];
-      let allData = await fetchData();
-
-      allData.forEach((item) => {
-        if (
-          item.category === "men's clothing" ||
-          item.category === "women's clothing"
-        ) {
-          reqData.push(item);
-        }
-      });
-
-      setProducts(reqData);
-    }
-
-    loadData();
-  }, []);
-
-  console.log("from store", products);
+  console.log("from store", props.products);
 
   return (
     <div id="store">
-      <CardContainer products={products} handleClick={props.handleAddtoCart}/>
+      <CardContainer products={props.products} handleClick={props.handleAddtoCart}/>
     </div>
   );
 };
