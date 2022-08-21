e933164cbec3cc681fc1231848a7e85cb4d94cbf
OhmPatil <workpatil10@gmail.com>
2022-08-21 15:32:27 +0530

    Update footer and some minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import '../styles/footer.css'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
     return(
         <div id='footer'>
-            <p>Made by Ohm Patil</p>
+            <p id='footer-text'>Made by Ohm Patil<a id='footer-logo' href='https://github.com/OhmPatil' target='_blank' rel='noopener noreferrer'><FaGithub size={'1.5rem'}/></a></p>
         </div>
     )
 }
