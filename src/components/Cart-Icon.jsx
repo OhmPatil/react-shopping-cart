97a86c7afcb1b7b93455c7312a2fb06b7dee4b52
OhmPatil <workpatil10@gmail.com>
2022-09-29 11:16:42 +0530

    Updated cart header icon

--- src/components/Cart-Icon.jsx
+++ src/components/Cart-Icon.jsx
@@ -1,11 +1,11 @@
-import { FaShoppingCart } from "react-icons/fa";
+import { BsCart3 } from "react-icons/bs";
 import "../styles/cart-icon.css";
 
 const CartIcon = (props) => {
   return (
     <div id="container">
-      <FaShoppingCart size="1.5rem" />
-      {props.amount !== 0 && <div id="amount">{props.amount}</div>}
+      <BsCart3 size="1.5rem" />
+      <div id="amount"><p>{props.amount}</p></div>
     </div>
   );
 };
